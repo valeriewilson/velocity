d7f95ce88ac489f465b93e67401778749d31808f
vagrant <vagrant@vagrant.vm>
2016-11-28 01:24:00 +0000

    Added 2 Flask integration tests

--- test.py
+++ test.py
@@ -56,7 +56,14 @@ class FlaskTestsLoggedIn(TestCase):
 
         result = self.client.get("/")
         self.assertEqual(result.status_code, 200)
-        self.assertIn('<h2>Bike Route Inputs</h2>', result.data)
+        self.assertIn('<h3>Route Creator</h3>', result.data)
+
+    def test_routes_route(self):
+        """ Verify that routes page renders correctly """
+
+        result = self.client.get("/routes")
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('<h3>Filter Options</h3>', result.data)
 
 
 class FlashTestLoggingIn(TestCase):
@@ -88,6 +95,19 @@ class FlashTestLoggingIn(TestCase):
                                   follow_redirects=True)
 
         self.assertIn("Successfully logged in", result.data)
+
+    def test_register_route_post(self):
+        """ Verify that register page processes data correctly """
+
+        result = self.client.post("/register",
+                                  data={"email": "test@client.com",
+                                        "password": "test123",
+                                        "first-name": "Test",
+                                        "last-name": "Client",
+                                        "phone-number": "1231231234"},
+                                  follow_redirects=True)
+
+        self.assertIn("Successfully registered", result.data)
 
 
 if __name__ == "__main__":
