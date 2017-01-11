3c040fb21a9215a0a263e067e30cac908182daad
vagrant <vagrant@vagrant.vm>
2017-01-11 18:04:41 +0000

    Updated chart color scheme in anticipation of release

--- static/home.js
+++ static/home.js
@@ -16,14 +16,14 @@ function displayChart(result) {
             labels: ["N/NE", "NE/E", "E/SE", "SE/S", "S/SW", "SW/W", "W/NW", "NW/N"],
             datasets: [{
                 backgroundColor: [
-                    "#2ecc71",
-                    "#3498db",
-                    "#95a5a6",
-                    "#9b59b6",
-                    "#f1c40f",
-                    "#e74c3c",
-                    "#34495e",
-                    "#2ecc71"
+                    "#9BA5D0",
+                    "#DEB85F",
+                    "#B1DB68",
+                    "#914B9B",
+                    "#4B5A97",
+                    "#FFE9B5",
+                    "#10700D",
+                    "#C592CC"
                 ],
               data: [stats[45], stats[0], stats[315], stats[270], stats[225], stats[180], stats[135], stats[90]]
             }]
